54b28c493908ca16fbeb386b3e9d2bfb06f5e7d7
David Paz <dpazruiz@ucsd.edu>
2021-11-16 08:17:57 +0000

    working opencv version

--- include/rb5_utils/rb_camera.h
+++ include/rb5_utils/rb_camera.h
@@ -7,25 +7,45 @@
 #include <gst/gst.h>
 #include <glib.h>
 #include <fastcv.h>
+#include "opencv2/opencv.hpp"
+#include <cv_bridge/cv_bridge.h>
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/CompressedImage.h"
 
 class RbCamera{
   public:
     RbCamera();
+    ~RbCamera();
 
-    void processData();
+    // data
+    typedef struct _CustomData
+    {
+      GstElement *pipeline;
+      GstElement *source;
+      GstElement *convert;
+      GstElement *filter;
+      GstElement *tee;
+      GstElement *appsink;
+      GstElement *videosink;
+    } CustomData;
 
-    ros::NodeHandle n;
-    ros::Publisher cam_pub;
+    void init();
+    //static GstFlowReturn processData(GstElement * sink, CustomData * data);
+
+    //ros::NodeHandle n;
+    //ros::Publisher cam_pub;
 
     // Gstream
-    GMainLoop *loop;
-    GstElement *pipeline, *source, *framefilter, *sink;
+    //GMainLoop *loop;
+    //GstElement *pipeline, *source, *convert, *frame_filter, *tee, *sink, *video_sink;
+    GstMessage *msg;
     GstBus *bus;
+    GstStateChangeReturn ret;
     guint bus_id;
     gint8 camera_id;
 
+
+    CustomData data;
 };
 
 #endif
--- src/rb_camera_node.cpp
+++ src/rb_camera_node.cpp
@@ -1,51 +1,173 @@
 #include "rb_camera.h"
 
 
-RbCamera::RbCamera(){
-  cam_pub = n.advertise<sensor_msgs::Image>("camera", 10);
+ros::Publisher cam_pub;
 
-  gst_init(0, nullptr);
-  loop = g_main_loop_new(NULL, FALSE);
+static GstFlowReturn processData(GstElement * sink, RbCamera::CustomData * data){
+  sensor_msgs::Image cam_msg;
+  cv_bridge::CvImage bridge;
 
-  pipeline    = gst_pipeline_new("video-display");
-  source      = gst_element_factory_make("qtiqmmfsrc", "qmmf-source");
-  framefilter = gst_element_factory_make("capsfilter", "frame-filter");
-  sink        = gst_element_factory_make("waylandsink","display");
-  if (!pipeline || !source || !framefilter || !sink) {
-    g_printerr ("Create element failed.\n");
+  GstSample *sample;
+  GstBuffer *app_buffer, *buffer;
+  GstMapInfo map_info;
+
+  // Retrieve buffer
+  g_signal_emit_by_name (sink, "pull-sample", &sample);
+
+  if(sample){
+    buffer = gst_sample_get_buffer (sample);
+    GstCaps *caps = gst_sample_get_caps(sample);
+
+    if (!gst_buffer_map ((buffer), &map_info, GST_MAP_READ)) {
+      gst_buffer_unmap ((buffer), &map_info);
+      gst_sample_unref(sample);
+
+      return GST_FLOW_ERROR;
+    }
+
+    cv::Mat frame_rgb = cv::Mat::zeros(480, 640, CV_8UC3);
+    cv::Mat frame(cv::Size(640, 480), CV_8UC4, (char*)map_info.data, cv::Mat::AUTO_STEP);
+    cv::cvtColor(frame, frame_rgb, cv::COLOR_RGBA2BGR);
+    std_msgs::Header header;
+    header.seq = 0;
+    header.stamp = ros::Time::now();
+    bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, frame_rgb);
+    bridge.toImageMsg(cam_msg);
+    
+
+
+    // Publish camera frame and free resources
+    gst_sample_unref(sample);
+    cam_pub.publish(cam_msg);
+
+    return GST_FLOW_OK;
   }
 
-  g_object_set (G_OBJECT(source), "camera", 0, nullptr);
+  //fcvOperationMode mode = FASTCV_OP_PERFORMANCE;
+  //fcvSetOperationMode(mode);
+  return GST_FLOW_ERROR;
 
-  if (camera_id == 0) {
-    g_object_set(G_OBJECT(framefilter), "caps",
-                 gst_caps_from_string("video/x-raw,format=NV12,framerate=30/1,width=1920,height=1080"), nullptr);
-  }
-  g_object_set (G_OBJECT(sink), "x", 0, "y", 200, "width", 640, "height", 360, nullptr);
-  bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
 
 }
 
+RbCamera::RbCamera(){
 
-void RbCamera::processData(){
-  sensor_msgs::Image cam_msg;
-  //fcvOperationMode mode = FASTCV_OP_PERFORMANCE;
-  //fcvSetOperationMode(mode);
+  gst_init(0, nullptr);
+  camera_id = 0;
+  //loop = g_main_loop_new(NULL, FALSE);
 
-  cam_pub.publish(cam_msg);
+  data.source      = gst_element_factory_make("qtiqmmfsrc", "source");
+  data.convert = gst_element_factory_make ("videoconvert", "convert");
+  data.appsink = gst_element_factory_make ("appsink", "sink");
+  data.videosink = gst_element_factory_make ("autovideosink", "sink");
+  data.filter = gst_element_factory_make("vertigotv", "filter");
+  //data.filter = gst_element_factory_make("capsfilter", "filter");
+  data.tee = gst_element_factory_make ("tee", "tee");
+  data.pipeline    = gst_pipeline_new("rb5-camera");
 
+  if (!data.pipeline || !data.convert || !data.source || !data.appsink ||
+      !data.filter || !data.videosink) {
+    g_printerr ("Not all elements could be created.\n");
+    return;
+  }
+
+
+  // Build pipeline
+  gst_bin_add_many (GST_BIN (data.pipeline), data.source, data.convert, data.filter, data.appsink, nullptr);
+  if (gst_element_link (data.source, data.convert) != TRUE ||
+      gst_element_link (data.convert, data.filter) != TRUE ||
+      gst_element_link (data.filter, data.appsink) != TRUE) {
+    g_printerr ("Elements could not be linked.\n");
+    gst_object_unref (data.pipeline);
+    return;
+  }
+
+
+
+  //if (camera_id == 0) {
+  //  g_object_set(G_OBJECT(data.filter), "caps",
+  //               gst_caps_from_string("video/x-raw,format=NV12,framerate=30/1,width=1920,height=1080"), nullptr);
+  //}
+  //else{
+  //  g_object_set(G_OBJECT(data.filter), "caps",
+  //               gst_caps_from_string("video/x-raw,format=NV12,framerate=30/1,width=1280,height=720"), nullptr);
+  //}
+
+  //g_object_set (G_OBJECT(data.source), "camera", 0, nullptr);
+  //g_object_set (G_OBJECT(sink), "x", 0, "y", 200, "width", 640, "height", 360, nullptr);
+  //bus = gst_pipeline_get_bus(GST_PIPELINE(data.pipeline));
+  //
+
+}
+
+RbCamera::~RbCamera(){
+  gst_object_unref(bus);
+  gst_element_set_state(data.pipeline, GST_STATE_NULL);
+  gst_object_unref(data.pipeline);
+}
+
+void RbCamera::init(){
+  g_object_set(data.appsink, "emit-signals", TRUE, nullptr);
+  //g_object_set(G_OBJECT(data.source), "camera", camera_id, NULL);
+  //g_object_set(G_OBJECT(data.filter), "caps",
+  //             gst_caps_from_string("video/x-raw,format=NV12,framerate=30/1,width=1920,height=1080"), nullptr);
+  g_signal_connect(data.appsink, "new-sample", G_CALLBACK(processData), &data);
+
+  // play
+  ret = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
+  if(ret == GST_STATE_CHANGE_FAILURE){
+    g_printerr ("Unable to set the pipeline to the playing state.\n");
+    gst_object_unref (data.pipeline);
+    return;
+  }
+  bus = gst_element_get_bus (data.pipeline);
+
+    /* Wait until error or EOS */
+  bus = gst_element_get_bus (data.pipeline);
+  msg =
+      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
+      (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+  /* Parse message */
+  if (msg != NULL) {
+    GError *err;
+    gchar *debug_info;
+
+    switch (GST_MESSAGE_TYPE (msg)) {
+      case GST_MESSAGE_ERROR:
+        gst_message_parse_error (msg, &err, &debug_info);
+        g_printerr ("Error received from element %s: %s\n",
+            GST_OBJECT_NAME (msg->src), err->message);
+        g_printerr ("Debugging information: %s\n",
+            debug_info ? debug_info : "none");
+        g_clear_error (&err);
+        g_free (debug_info);
+        break;
+      case GST_MESSAGE_EOS:
+        g_print ("End-Of-Stream reached.\n");
+        break;
+      default:
+        /* We should not reach here because we only asked for ERRORs and EOS */
+        g_printerr ("Unexpected message received.\n");
+	break;
+    }
+    gst_message_unref (msg);
+  }
 }
 
 int main(int argc, char *argv[]){
   ros::init(argc, argv, "rb_camera");
+  ros::NodeHandle n;
+  cam_pub = n.advertise<sensor_msgs::Image>("camera", 10);
   RbCamera cam;
+  cam.init();
 
   //ros::spin();
-  ros::Rate rate(10.0);
-  while(ros::ok()){
-    cam.processData();
-    rate.sleep();
-  }
-  return 0;
+  //ros::Rate rate(10.0);
+  //while(ros::ok()){
+  //  cam.processData();
+  //  rate.sleep();
+  //}
+  //return 0;
 }
 
