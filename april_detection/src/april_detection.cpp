1fea63b46e1be91900dfc499f7f3901e30a52e90
David Paz <dpazruiz@ucsd.edu>
2021-12-02 18:38:21 +0000

    apriltag detector running

--- april_detection/src/april_detection.cpp
+++ april_detection/src/april_detection.cpp
@@ -7,17 +7,20 @@ AprilDetection::AprilDetection(){
   // tag36h11
   tf = tag36h11_create();
 
-  getopt_t *getopt = getopt_create();
-  const char *famname = getopt_get_string(getopt, "family");
+  //getopt_t *getopt = getopt_create();
+  //const char *famname = getopt_get_string(getopt, "family");
 
   // configure detector
-  apriltag_detector_add_family_bits(a_detector, tf, getopt_get_int(getopt, "hamming"));
+  //apriltag_detector_add_family_bits(a_detector, tf, getopt_get_int(getopt, "hamming"));
 
-  a_detector->quad_decimate = getopt_get_double(getopt, "decimate");
-  a_detector->quad_sigma = getopt_get_double(getopt, "blur");
-  a_detector->nthreads = getopt_get_int(getopt, "threads");
-  a_detector->debug = getopt_get_bool(getopt, "debug");
-  a_detector->refine_edges = getopt_get_bool(getopt, "refine-edges");
+  //a_detector->quad_decimate = getopt_get_double(getopt, "decimate");
+  //a_detector->quad_sigma = getopt_get_double(getopt, "blur");
+  //a_detector->nthreads = getopt_get_int(getopt, "threads");
+  //a_detector->debug = getopt_get_bool(getopt, "debug");
+  //a_detector->refine_edges = getopt_get_bool(getopt, "refine-edges");
+
+  apriltag_detector_add_family(a_detector, tf);
+  
 
   return;
 }
@@ -40,8 +43,10 @@ cv::Mat AprilDetection::processImage(cv::Mat image){
   zarray_t * detections = apriltag_detector_detect(a_detector, &im);
 
   apriltag_detection_t *det;
+  ROS_INFO("Processing AprilTag");
   for (int i=0; i<zarray_size(detections); i++){
     zarray_get(detections, i, &det);
+    // TODO: estimate pose 
   }
   
   return image;
--- april_detection/src/april_detection_node.cpp
+++ april_detection/src/april_detection_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <ros/console.h>
+#include <cv_bridge/cv_bridge.h>
 #include "april_detection.h"
 #include "sensor_msgs/Image.h"
 #include "geometry_msgs/PoseStamped.h"
@@ -10,6 +11,11 @@ AprilDetection det;
 
 void imageCallback(const sensor_msgs::Image::ConstPtr& msg){
   ROS_INFO("image received");
+  cv_bridge::CvImagePtr img_cv = cv_bridge::toCvCopy(msg);
+
+  // run detection
+  det.processImage(img_cv->image);
+
   geometry_msgs::PoseStamped pose_msg;
   pose_pub.publish(pose_msg);
 }
@@ -21,5 +27,8 @@ int main(int argc, char *argv[]){
 
   pose_pub = n.advertise<geometry_msgs::PoseStamped>("/april_poses", 10); 
   image_sub = n.subscribe("/camera_0", 1, imageCallback);
+  
+  ros::spin();
+  return 0;
   
 }
