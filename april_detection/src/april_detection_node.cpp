a284c173ee0f5f93dba50d251bd53fc64cac368a
David Paz <dpazruiz@ucsd.edu>
2021-12-02 10:16:07 -0800

    adds main ros node callbacks

--- april_detection/src/april_detection_node.cpp
+++ april_detection/src/april_detection_node.cpp
@@ -1,5 +1,24 @@
+#include <ros/ros.h>
+#include <ros/console.h>
 #include "april_detection.h"
+#include "sensor_msgs/Image.h"
+#include "geometry_msgs/PoseStamped.h"
 
-int main(){
-  AprilDetection det;
+ros::Publisher pose_pub;
+ros::Subscriber image_sub;
+AprilDetection det;
+
+void imageCallback(sensor_msgs::Image::ConstPtr& msg){
+  ROS_INFO("image received");
+  geometry_msgs::PoseStamped pose_msg;
+  pose_pub.publish(pose_msg);
+}
+int main(int argc, char *argv[]){
+  ros::init(argc, argv, "april_detection");
+  ros::NodeHandle n;
+  ros::NodeHandle private_nh("~");
+
+  pose_pub = n.advertise<geometry_msgs::PoseStamped>("/april_poses", 10); 
+  image_sub = n.subscribe<sensor_msgs::Image>("/camera_0", 1, imageCallback);
+  
 }
